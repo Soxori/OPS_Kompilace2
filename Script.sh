f59b80a82e2dad56c7490ebe9a7b041d18c40970
Jakub Panek <jakub.panek@educanet.cz>
2020-09-22 18:16:04 +0200

    asidone

---
+++ Script.sh
@@ -0,0 +1,10 @@
+cd C:/Users/jakub/Desktop/OPS/OPS_Ukol/OPS_Kompilace2
+
+git clone https://github.com/EduTeachers/HelloWorld
+
+cd C:/Users/jakub/Desktop/OPS/OPS_Ukol/OPS_Kompilace2/HelloWorld/src/com/company
+
+javac Main.java
+java Main > compiler_out.txt
+java Main 2> compiler_error.txt
+java Main &> runtime_out.txt
